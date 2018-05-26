bc3b623d84d9e5c0a1a450509d2611ff40956992
Mitchell Durbin <dude0413@gmail.com>
2018-05-26 19:50:30 -0400

    Update setupToolsKali.sh

--- setupToolsKali.sh
+++ setupToolsKali.sh
@@ -1,14 +1,7 @@
-# Regular Colors
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
-NC='\033[0m' # No Color
-
+Red='tput setaf 1'
+Green='tput setaf 2'
+Blue='tput setaf 4'
+NC='tput sgr0'
 
 printf "${Red}Starting up${NC}"
 echo 
@@ -175,6 +168,14 @@ cd /opt/Veil && ./Install.sh -c
 printf "${Green}metagoofil${NC}"
 echo
 git clone https://github.com/laramies/metagoofil /opt/metagoofil
+
+printf "${Green}AutoSploit${NC}"
+echo
+git clone https://github.com/NullArray/AutoSploit /opt/AutoSploit
+
+printf "${Green}AverageSecGuyScripts${NC}"
+echo
+git clone https://github.com/averagesecurityguy/scripts /opt/avgscripts
 
 printf "${Red}Finishing Up!${NC}"
 echo

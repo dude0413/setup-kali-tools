178c6bd2aca269bf650750407c09f6b91a4bd9d9
Mitchell Durbin <dude0413@gmail.com>
2018-05-26 20:11:40 -0400

    Update setupToolsKali.sh

--- setupToolsKali.sh
+++ setupToolsKali.sh
@@ -1,91 +1,75 @@
-Red='tput setaf 1'
-Green='tput setaf 2'
-Blue='tput setaf 4'
-NC='tput sgr0'
+echo -e "$(tput setaf 1)Starting up$(tput sgr0)"
 
-printf "${Red}Starting up${NC}"
-echo 
-printf "${Blue}Updating the system${NC}"
-echo
+echo -e "$(tput setaf 4)Updating the system$(tput sgr0)"
 apt-get upgrade
 apt-get dist-upgrade
 apt-get update
 
-printf "${Blue}Updating PIP"
+echo -e "$(tput setaf 4)Updating PIP"
 pip install --upgrade pip
 
-
 echo 'Starting to download extra tools'
-echo
 
-printf "${Green}The Backdoor Factory${NC}"
-echo
+echo -e "$(tput setaf 2)The Backdoor Factory$(tput sgr0)"
 git clone https://github.com/secretsquirrel/the-backdoor-factory /opt/the-backdoor-factory
 cd /opt/the-backdoor-factory
 chmod 755 install.sh && ./install.sh
 
-printf "${Green}HTTP ScreenShot${NC}"
-echo
+echo -e "$(tput setaf 2)HTTP ScreenShot$(tput sgr0)"
 pip install selenium
 git clone https://github.com/breenmachine/httpscreenshot.git /opt/httpscreenshot
 cd /opt/httpscreenshot 
 chmod 755 install-dependencies.sh && ./install-dependencies.sh
 
-printf '${Green}SMBExec${NC}'
-echo
+echo -e '$(tput setaf 2)SMBExec$(tput sgr0)'
 git clone https://github.com/pentestgeek/smbexec.git /opt/smbexec
 cd /opt/smbexec
 ./install.sh
 
-printf '${Green}Masscan${NC}'
-echo
+echo -e '$(tput setaf 2)Masscan$(tput sgr0)'
 apt-get install git gcc make libpcap-dev
 git clone https://github.com/robertdavidgraham/masscan.git /opt/masscan
 cd /opt/masscan
 make 
 make install
 
-printf "${Green}Gitrob${NC}"
-echo
+echo -e "$(tput setaf 2)Gitrob$(tput sgr0)"
 git clone https://github.com/michenriksen/gitrob.git /opt/gitrob
 gem install bundler
 
-printf "${Green}CMSmap${NC}"
-echo
+echo -e "$(tput setaf 2)CMSmap$(tput sgr0)"
 git clone https://github.com/Dionach/CMSmap /opt/CMSmap
 
-printf "${Green}WPScan${NC}"
-echo
+echo -e "$(tput setaf 2)WPScan$(tput sgr0)"
 git clone https://github.com/wpscanteam/wpscan.git /opt/wpscan 
 
-printf "${Green}EyeWitness${NC}"
-echo
+echo -e "$(tput setaf 2)EyeWitness$(tput sgr0)"
 git clone https://github.com/ChrisTruncer/EyeWitness.git /opt/EyeWitness
 
-printf "${Green}Printer Exploits${NC}"
+echo -e "$(tput setaf 2)Printer Exploits$(tput sgr0)"
 echo
 git clone https://github.com/MooseDojo/praedasploit /opt/praedasploit
 
-printf "${Green}SQLMap${NC}"
+echo -e "$(tput setaf 2)SQLMap$(tput sgr0)"
 echo
 git clone https://github.com/sqlmapproject/sqlmap /opt/sqlmap 
 
-printf "${Green}Discover Scripts${NC}"
+echo -e "$(tput setaf 2)Discover Scripts$(tput sgr0)"
 echo
 git clone https://github.com/leebaird/discover.git /opt/discover 
 
-printf "${Green}BeEF${NC}"
+echo -e "$(tput setaf 2)BeEF$(tput sgr0)"
 echo
 cd /opt
 wget https://raw.github.com/beefproject/beef/a6a7536e/install-beef 
 chmod 755 install-beef
 ./install-beef
 
-printf "${Green}Responder${NC}"
+echo -e "$(tput setaf 2)Responder$(tput sgr0)"
 echo
 git clone https://github.com/SpiderLabs/Responder.git /opt/Responder
 
-printf "${Green}Custom Scripts${NC}"
+echo -e "${Yellow}Custom Scripts$(tput sgr0)"
 echo
 git clone https://github.com/cheetz/Easy_P.git /opt/Easy_P
 git clone https://github.com/cheetz/Passsword_Plus_One /opt/Passowrd_Plus_One
@@ -94,17 +78,17 @@ git clone https://github.com/cheetz/icmpshock /opt/icmpshock
 git clone https://github.com/cheetz/brutescrape /opt/brutescrape
 git clone https://github.com/cheetz/reddit_xss /opt/reddit_xss
 
-printf "${Green}Forked Versions${NC}"
+echo -e "$(tput setaf 2)Forked Versions$(tput sgr0)"
 echo
 git clone https://github.com/cheetz/PowerSploit /opt/HP_PowerSploit
 git clone https://github.com/cheetz/PowerTools /opt/PowerTools
 git clone https://github.com/cheetz/nishang /opt/nishang
 
-printf "${Green}DSHashes${NC}"
+echo -e "$(tput setaf 2)DSHashes$(tput sgr0)"
 echo
 wget http://ptscripts.googlecode.com/svn/trunk/dshashes.py -O /opt/NTDSXtract/dshashes.py
 
-printf "${Green}NoSQLMap${NC}"
+echo -e "$(tput setaf 2)NoSQLMap$(tput sgr0)"
 echo
 git clone https://github.com/tcstool/NoSQLMap.git /opt/NoSQLMap
 pip install ipcalc
@@ -112,7 +96,7 @@ pip install pbkdf2
 pip install couchdb
 pip install 
 
-printf "${Green}Spiderfoot${NC}"
+echo -e "$(tput setaf 2)Spiderfoot$(tput sgr0)"
 echo
 mkdir /opt/spiderfoot && cd /opt/spiderfoot
 wget http://sourceforge.net/projects/spiderfoot/files/spiderfoot-2.3.0-src.tar.gz/download 
@@ -123,59 +107,59 @@ pip install M2Crypto
 pip install cherrypy
 pip install mako
 
-printf "${Green}Mimikatz${NC}"
+echo -e "$(tput setaf 2)Mimikatz$(tput sgr0)"
 echo
 git clone https://github.com/gentilkiwi/mimikatz /opt/mimikatz
 
-printf "${Green}PowerSploit${NC}"
+echo -e "$(tput setaf 2)PowerSploit$(tput sgr0)"
 echo
 git clone https://github.com/mattifestation/PowerSploit.git /opt/PowerSploit
 cd /opt/PowerSploit && wget https://raw.githubusercontent.com/obscuresec/random/master/StartListener.py && wget https://raw.githubusercontent.com/darkoperator/powershell_scripts/master/ps_encoder.py
 
-printf "${Green}Nishang${NC}"
+echo -e "$(tput setaf 2)Nishang$(tput sgr0)"
 echo
 git clone https://github.com/samratashok/nishang /opt/nishang
 
-printf "${Green}Wifite${NC}"
+echo -e "$(tput setaf 2)Wifite$(tput sgr0)"
 echo
 git clone https://github.com/derv82/wifite /opt/wifite
 
-printf "${Green}SecLists${NC}"
+echo -e "$(tput setaf 2)SecLists$(tput sgr0)"
 echo
 git clone https://github.com/danielmiessler/SecLists.git /opt/SecLists
 
-printf "${Green}WIFIPhisher${NC}"
+echo -e "$(tput setaf 2)WIFIPhisher$(tput sgr0)"
 echo
 git clone https://github.com/sophron/wifiphisher.git /opt/wifiphisher
 
-printf "${Green}gitlist${NC}"
+echo -e "$(tput setaf 2)gitlist$(tput sgr0)"
 echo
 git clone https://github.com/macubergeek/gitlist.git /opt/gitlist
 
-printf "${Green}Phishing-Frenzy${NC}"
+echo -e "$(tput setaf 2)Phishing-Frenzy$(tput sgr0)"
 echo
 git clone https://github.com/pentestgeek/phishing-frenzy.git /var/www/phishing-frenzy
 
-printf "${Green}net-creds${NC}"
+echo -e "$(tput setaf 2)net-creds$(tput sgr0)"
 echo
 git clone https://github.com/DanMacInerney/net-creds.git /opt/net-creds
 
-printf "${Green}Veil${NC}"
+echo -e "$(tput setaf 2)Veil$(tput sgr0)"
 echo
 git clone https://github.com/Veil-Framework/Veil /opt/Veil
 cd /opt/Veil && ./Install.sh -c
 
-printf "${Green}metagoofil${NC}"
+echo -e "$(tput setaf 2)metagoofil$(tput sgr0)"
 echo
 git clone https://github.com/laramies/metagoofil /opt/metagoofil
 
-printf "${Green}AutoSploit${NC}"
+echo -e "$(tput setaf 2)AutoSploit$(tput sgr0)"
 echo
 git clone https://github.com/NullArray/AutoSploit /opt/AutoSploit
 
-printf "${Green}AverageSecGuyScripts${NC}"
+echo -e "$(tput setaf 2)AverageSecGuyScripts$(tput sgr0)"
 echo
 git clone https://github.com/averagesecurityguy/scripts /opt/avgscripts
 
-printf "${Red}Finishing Up!${NC}"
+echo -e "$(tput setaf 1)Finishing Up!$(tput sgr0)"
 echo
